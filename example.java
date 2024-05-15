9969a2b66773ffb31b7648ea2e260948b1e63a75
SergeiP-JS <73941080+SergeiP-JS@users.noreply.github.com>
2024-05-16 02:00:22 +0500

    Add files via upload

--- example.java
+++ example.java
@@ -1,41 +1,25 @@
-
-
-
-package com.company;
-//
-
-
-
-
-
+package com.company;
+//comment
 public class Main {
-
-
     int value=10+(7+5)*9;
-
-
+    
     boolean isActive = false;
     boolean isAlive = true;
+    
     int binaryInt = 0b101;
-
-    int x=0b101;
-
-
-
+    
     int decimal = 495;
     int x_b = 0b101;
     int x_octal = 0757;
     int x_hex = 0XFF;
-
-
     float fl = 130.45E-2F;
     int x_null = null;
 
-    int y_=0b110;
-
     int z=x+y;
     byte a_1 = 3;
-    byte b_1 = 8;
+    byte b_1 = 8;
+    char a_5 = '\u03a9';
+    char b_5 = null;
 
     short a_2 = 3;
     short b_2 = 8;
@@ -46,49 +30,7 @@ public class Main {
     long a_4 = 5;
     long b_4 = 10;
 
-
-
-    double y = 2.7d;
-
-    char a_5 = '\u03a9';
-    char b_5 = null;
-    
-
-
-
-
-
+    double y = 2.7d;    
     float x = 8.5F;
     float y = 2.7F;
-
-    
-   
-
 }
-
-
-
-//class PointK
-//{
-//
-//    int x, y;
-//    PointK(int x, int y)
-//    {
-//
-//        this.x = x;
-//        this.y = y;
-//    }
-//}
-//
-//
-//class TwoPointsInitK
-//{
-//
-//    public static void main(String args[])
-//        {
-//            PointK p1 = new PointK(10,20);
-//            PointK p2 = new PointK(42,99);
-//            System.out.println("x = " + p1.x + " y = " + p1.y);
-//            System.out.println("x = " + p2.x + " y = " + p2.y);
-//        }
-//}
